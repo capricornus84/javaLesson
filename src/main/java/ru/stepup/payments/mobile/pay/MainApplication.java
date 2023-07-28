101cb0b9bcbfb0a89fd5bcce8c8e51ed5694996a
capricornus84 <capricornus@inbox.ru>
2023-07-28 21:51:02 +0300

    второй комит комит

--- src/main/java/ru/stepup/payments/mobile/pay/MainApplication.java
+++ src/main/java/ru/stepup/payments/mobile/pay/MainApplication.java
@@ -3,5 +3,6 @@ package ru.stepup.payments.mobile.pay;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Welcome to Mobile Pay");
+        //test text
     }
 }
